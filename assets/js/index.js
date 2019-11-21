4592f776d3ebc9687303c7dff94938e7af02816e
Scout <therearespidersinthisemail@gmail.com>
2019-11-20 18:45:54 -0700

    finished nav bar and header as well as menu

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+function openNav() {
+    document.getElementById("curtainNav").style.height = "100%";
+
+}
+
+function closeNav() {
+    document.getElementById("curtainNav").style.height = "0%";
+}
